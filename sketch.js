cca9719a0fb28ba4f83d6a15c4ec2f3369918305
YY31-z <yuyanzang@gmail.com>
2025-05-06 13:12:46 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,33 @@
+let copie = 50;
+let cubi = [];
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  for (let i = 0; i < copie; i++) {
+    let cubo = {
+      x: random(-2000, 2000),
+      y: random(-1000, 1000),
+      z: random(-1000, 1000),
+      size: 100,
+      colore: random(["pink", "yellow", "green"]),
+      rotationFunction: random([rotateX, rotateY, rotateZ]),
+    };
+    cubi.push(cubo);
+  }
 }
 
 function draw() {
-  background(220);
-  const s = 100;
-
-  rect(0, 0, s, s);
-  rect(0, height - s, s, s);
-  rect(width - s, 0, s, s);
-  rect(width - s, height - s, s, s);
+  background("black");
+  orbitControl();
+  for (let cubo of cubi) {
+    push();
+    translate(cubo.x, cubo.y, cubo.z);
+    cubo.rotationFunction(frameCount / 10);
+    fill(cubo.colore);
+    box(cubo.size);
+    pop();
+  }
 }
 
 function windowResized() {
